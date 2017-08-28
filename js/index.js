92baad663b7986aa6f4ff0b0e819aa4e8879241a
Bhargav Rao <bhargav.max@gmail.com>
2017-08-28 09:07:44 +0530

    Inheritance hack project

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		if($('#mySidenav').css('width') === '400px')
+			$('#mySidenav').css('width', '0px');
+		else 
+			$('#mySidenav').css('width', '400px');
+	});
+
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
